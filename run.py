f0804ccc0d2bde8d11c070d12f8f2b5f6293f135
Saurabh Khade <khadesaurabh056@gmail.com>
2022-06-14 22:27:19 +0530

    TFIDF Vectorizer replace by CountVectorizer in title search

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
-from email.mime import application
 from flask import Flask
 from flask_restful import Api
 from app.home import Home
 from app.search import Search
 from app.recommend import Recommend
 from app.data_bundle import Data
-from gevent.pywsgi import WSGIServer
 from flask_cors import CORS
 
-application = Flask(__name__)
-CORS(application)
-api = Api(application)
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
 
 api.add_resource(Home, '/')
 api.add_resource(Search, '/search')
@@ -18,5 +16,4 @@ api.add_resource(Recommend, '/recommend')
 api.add_resource(Data, '/data')
 
 if __name__ == '__main__':
-    # WSGIServer(('', 5000), application).serve_forever()
-    application.run(debug=True)
+    app.run(debug=True)
