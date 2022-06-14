89aa27f0b3923676d951e9b2dfa211ad27a43857
Saurabh Khade <khadesaurabh056@gmail.com>
2022-06-15 03:16:18 +0530

    frontend added

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from app.home import Home
 from app.search import Search
@@ -6,15 +6,20 @@ from app.recommend import Recommend
 from app.data_bundle import Data
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend')
 app.config['JSON_SORT_KEYS'] = False
 CORS(app)
 api = Api(app)
 
-api.add_resource(Home, '/')
 api.add_resource(Search, '/search')
 api.add_resource(Recommend, '/recommend')
 api.add_resource(Data, '/data')
+
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
